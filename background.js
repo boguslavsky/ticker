52da9d8ae2895ef1218e7c3584df98792489e333
Ivan Boguslavsky <i.am.an.eternity@gmail.com>
2017-05-15 12:52:27 +0300

    simplified ticker functions

--- background.js
+++ background.js
@@ -1,24 +1,37 @@
-var INTERVAL = 5000;
+$(function() {
+    var INTERVAL = 10000;
+    var BASEURL = 'http://comediededante.net/api/v1/';
 
-function updateIcon() {
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    var img = new Image();
-    var value = 1248;
+    function updateIcon() {
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
 
-    img.src = 'icon.png';
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0, 32, 32);
-        ctx.font = 'bold 21px monospace';
-        ctx.fillStyle = '#fff';
-        ctx.textAlign = 'center';
-        ctx.fillText(value, 16, 22, 28);
+        $.ajax({
+            method: 'GET',
+            url: BASEURL + 'ticker/btc_usd',
+            success: function(response) {
+                try {
+                    var value = numeral(response.btc_usd.last).format('0,0.00');
+                    var img = new Image();
 
-        var imageData = ctx.getImageData(0, 0, 32, 32);
-        chrome.browserAction.setIcon({imageData: imageData});
-    };
+                    img.src = 'icon.png';
+                    img.onload = function() {
+                        ctx.drawImage(img, 0, 0, 32, 32);
+                        ctx.font = 'bold 21px monospace';
+                        ctx.fillStyle = '#fff';
+                        ctx.textAlign = 'center';
+                        ctx.fillText(value, 16, 22, 28);
+
+                        var imageData = ctx.getImageData(0, 0, 32, 32);
+
+                        chrome.browserAction.setIcon({imageData: imageData});
+                    };
+                } catch (e) {}
+            }
+        });
+    }
+
+    updateIcon();
 
     setTimeout(updateIcon, INTERVAL);
-}
-
-document.addEventListener('DOMContentLoaded', updateIcon);
+});
--- popup.js
+++ popup.js
@@ -1,104 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(function() {
+    var INTERVAL = 5000;
+    var BASEURL = 'http://comediededante.net/api/v1/';
 
-    Highcharts.stockChart('chart', {
-        chart: {
-            spacing: [0, 0, 0, 0],
-            backgroundColor: '#272b37'
-        },
-        rangeSelector: {
-            enabled: false
-        },
-        scrollbar: {
-            enabled: false
-        },
-        navigator: {
-            enabled: false
-        },
-        navigation: {
-            buttonOptions: {
-                enabled: false
-            }
-        },
-        tooltip: {
-            enabled: false
-        },
-        credits: {
-            enabled: false
-        },
-        xAxis: {
-            lineColor: '#272b37',
-            title: {
-                enabled: false
-            },
-            labels: {
-                enabled: false
-            },
-            tickLength: 0
-        },
-        yAxis: {
-            gridLineColor: '#272b37',
-            title: {
-                enabled: false
-            },
-            labels: {
-                enabled: false
-            },
-            TickLength: 0
-        },
-        plotOptions: {
-            series: {
-                enableMouseTracking: false,
-                states: {
-                    hover: {
-                        enabled: false
-                    }
-                }
-            }
-        },
-        series: [{
-            data: [
-                [1268611200000, 31.98],
-                [1268697600000, 32.06],
-                [1268784000000, 32.02],
-                [1268870400000, 32.09],
-                [1268956800000, 31.75],
-                [1269216000000, 32.11],
-                [1269302400000, 32.62],
-                [1269388800000, 32.77],
-                [1269475200000, 32.38],
-                [1269561600000, 32.99],
-                [1269820800000, 33.20],
-                [1269907200000, 33.69],
-                [1269993600000, 33.57],
-                [1270425600000, 34.07],
-                [1270512000000, 34.22],
-                [1270598400000, 34.37],
-                [1270684800000, 34.28],
-                [1270771200000, 34.54],
-                [1271030400000, 34.61],
-                [1271116800000, 34.63],
-                [1271203200000, 35.10],
-                [1271289600000, 35.56],
-                [1271376000000, 35.34],
-                [1271635200000, 35.30],
-                [1271721600000, 34.94],
-                [1271808000000, 37.03],
-                [1271894400000, 38.07],
-                [1271980800000, 38.69],
-                [1272240000000, 38.50],
-                [1272326400000, 37.43],
-                [1272412800000, 37.37],
-                [1272499200000, 38.38],
-                [1272585600000, 37.30]
-            ],
-            type: 'area',
-            threshold: null,
-            tooltip: {
-                valueDecimals: 2
-            },
-            lineColor: '#373d50',
-            fillColor: false
-        }]
-    });
+    function setTicker(response, pairs) {
+        for (var i = 0, len = pairs.length; i < len; i++) {
+            var ticker = response[pairs[i]];
 
+            var $value = $('#' + pairs[i]);
+            $value.text('$' + numeral(ticker.last).format('0,0.00'));
+
+            var $trend = $('#' + pairs[i] + '-trend');
+            if (ticker.last > ticker.avg) {
+                $trend.addClass('growth').removeClass('drop');
+                $trend.text('+' + numeral(ticker.last - ticker.avg).format('0,0.00') + ' (' + numeral(100 * (ticker.last / ticker.avg - 1)).format('0.00') + '%)');
+            } else {
+                $trend.addClass('drop').removeClass('growth');
+                $trend.text('-' + numeral(Math.abs(ticker.last - ticker.avg)).format('0,0.00') + ' (' + numeral(100 * (1 - ticker.last / ticker.avg)).format('0.00') + '%)');
+            }
+        }
+    }
+
+    function loadInfo() {
+        $.ajax({
+            method: 'GET',
+            url: BASEURL + 'ticker/btc_usd-ltc_usd-dsh_usd-eth_usd',
+            success: function(response) {
+                try {
+                    setTicker(response, ['btc_usd', 'ltc_usd', 'dsh_usd', 'eth_usd']);
+                } catch (e) {}
+            }
+        });
+    }
+
+    loadInfo();
+
+    setTimeout(loadInfo, INTERVAL);
 });
